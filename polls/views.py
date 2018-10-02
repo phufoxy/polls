e4df1d4ae0cb6d9a8754f491f5cce844d0e8ab11
vanphudhsp2015 <vanphudhsp2015@gmail.com>
2018-10-02 16:58:42 +0700

    sds

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ def index(request):
     questions1 = Question.objects.order_by('-id')
     # dddddddddddddddddddddddddddddd
     # date
-    #sgjkjwrhnjghrfnjllfnfk
+    #sgjkjwrhnjghrfnjllsadsadsdsagitfnfk
     context1 = {
         'name':name1,
         'questions':questions1
