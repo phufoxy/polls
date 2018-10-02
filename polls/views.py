64babea5d6916ac9173d32749b54abc70819623e
HoangNi <36022163+HoangNi@users.noreply.github.com>
2018-10-02 17:04:52 +0700

    sdfh

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ def index(request):
     questions1 = Question.objects.order_by('-id')
     # dddddddddddddddddddddddddddddd
     # date
-    #sgjkjwrhnjghrfnjllsadsadsdsagishfhgdkbfjhdjfvtfnfk
+    #sgjkjwrhnjghrfnjllsadsadsdsagishfhgdkbfjhdjfvtfnfkdsdf
     context1 = {
         'name':name1,
         'questions':questions1
